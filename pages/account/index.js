c85e48a9405de8d4f054c66ee81a2a6d35aef97f
Ahmed (Onimisea) Tijani <onimisea@gmail.com>
2023-02-17 11:06:52 +0100

    Removed dependency variables from useEffec

--- pages/account/index.js
+++ pages/account/index.js
@@ -16,6 +16,8 @@ const Account = ({}) => {
   const router = useRouter();
   const [userStatus, setUserStatus] = useState(null);
 
+const [isUserStatus, setIsUserStatus] = useState(false);
+
   const {
     appState: { cart },
     tabbed,
@@ -124,7 +126,9 @@ const Account = ({}) => {
           }
         });
     }
-  }, [setUserInfo, userStatus]);
+  }, []);
+
+alert("Re-rendering?");
 
   return (
     <section className="w-[85%] mx-auto max-w-screen-xl">
